48b9184ae572a436dd96e6104fe81d5fd3fe044c
Luciano Mammino <lucianomammino@gmail.com>
2024-01-22 18:30:50 +0000

    Added logic to send a pong back

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Read, io::Write, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut buf: [u8; 1024] = [0; 1024];
+                let read_result = stream.read(&mut buf);
+                match read_result {
+                    Ok(_num_bytes) => {
+                        // TODO: verify that we are really receiving a ping
+                        let response = b"+PONG\r\n";
+                        let _write_result = stream.write(response);
+                    }
+                    Err(e) => {
+                        println!("error: {}", e);
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
